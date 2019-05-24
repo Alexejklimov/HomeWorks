dc8b432fc57eb10714f00da04e93814007cbbd93
UkrAndy <brila_andrij@ukr.net>
2019-05-24 12:06:32 +0300

    Передавайте якщо можна масив

--- 1components/src/App.js
+++ 1components/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Tovar title="vine" tovarProp={["Tsinandali", "250", "3650+", "6.5"]} />
       <Tovar title="vine" tovarProp={["Mukuzani", "270", "3650+", "5"]} />
       <hr />
-      <Kino time1="11:00" time2="13:00" time3="15:30" time1="19:45" />
+      <Kino time1="11:00" time2="13:00" time3="15:30" time1="19:45" />  Бажано передавати у таких випадках масив
 
     </div >
   );
